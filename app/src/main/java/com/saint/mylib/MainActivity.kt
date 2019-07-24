afbe4a86e6753eae89ce1441a6895991eae9eefa
saint_li <saint.li@qq.com>
2019-07-24 14:38:29 +0800

    权限申请添加未开启，去设置页面设置

--- app/src/main/java/com/saint/mylib/MainActivity.kt
+++ app/src/main/java/com/saint/mylib/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.saint.mylib
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
@@ -7,8 +8,25 @@ import android.os.Bundle
 import android.view.View
 import com.xuwuji.tuding.utillib.base.BaseAct
 import com.xuwuji.tuding.utillib.util.AppLog
+import com.xuwuji.tuding.utillib.util.AppToast
+import com.xuwuji.tuding.utillib.util.PermissionUtil
+import com.xuwuji.tuding.utillib.util.PermissionUtil.showSettingDialog
+import com.yanzhenjie.permission.Action
+import com.yanzhenjie.permission.AndPermission
+import com.yanzhenjie.permission.runtime.Permission
 
 class MainActivity : BaseAct() {
+
+    private val permissions = arrayOf(
+//        Permission.ACCESS_COARSE_LOCATION,
+//        Permission.ACCESS_FINE_LOCATION,
+//        Permission.READ_EXTERNAL_STORAGE,
+//        Permission.WRITE_EXTERNAL_STORAGE,
+//        Permission.READ_PHONE_STATE,
+//        Permission.CAMERA,
+//        Permission.CALL_PHONE,
+        Permission.RECORD_AUDIO
+    )
 
     override fun setLayout(): Int {
         return R.layout.activity_main
@@ -19,6 +37,7 @@ class MainActivity : BaseAct() {
     }
 
     override fun initData() {
+        requestPermission()
     }
 
 
@@ -33,6 +52,29 @@ class MainActivity : BaseAct() {
     fun click(v: View) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         startActivity(intent)
+    }
+
+
+    private fun requestPermission() {
+        PermissionUtil.request(act
+            , Action<List<String>> { data ->
+                if (AndPermission.hasAlwaysDeniedPermission(act, data)) {
+                    AppToast.tShort("全部权限已开启！")
+                }
+            }
+            , Action<List<String>> { data ->
+                if (AndPermission.hasAlwaysDeniedPermission(act, data)) {
+                    showSettingDialog(act, data, 0x1234)
+                }
+            }
+            , *permissions)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == 0x1234) {
+            requestPermission()
+        }
     }
 
 
