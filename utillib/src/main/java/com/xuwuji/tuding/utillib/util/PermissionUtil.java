573fef47e17fffabd166a2d060f9ce7745ba359f
saint_li <saint.li@qq.com>
2019-07-24 11:46:19 +0800

    修改权限申请依赖方式

--- utillib/src/main/java/com/xuwuji/tuding/utillib/util/PermissionUtil.java
+++ utillib/src/main/java/com/xuwuji/tuding/utillib/util/PermissionUtil.java
@@ -6,32 +6,34 @@ import android.support.v4.app.Fragment;
 import com.yanzhenjie.permission.Action;
 import com.yanzhenjie.permission.AndPermission;
 
+import java.util.List;
+
 public class PermissionUtil {
 
-    public static void request(Context context, Action onGranted, Action onDemied, String... permissions) {
+    public static void request(Context context, Action<List<String>> onGranted, Action<List<String>> onDenied, String... permissions) {
         AndPermission.with(context)
                 .runtime()
                 .permission(permissions)
                 .onGranted(onGranted)
-                .onDenied(onDemied)
+                .onDenied(onDenied)
                 .start();
     }
 
-    public static void request(Activity activity, Action onGranted, Action onDemied, String... permissions) {
+    public static void request(Activity activity, Action<List<String>> onGranted, Action<List<String>> onDenied, String... permissions) {
         AndPermission.with(activity)
                 .runtime()
                 .permission(permissions)
                 .onGranted(onGranted)
-                .onDenied(onDemied)
+                .onDenied(onDenied)
                 .start();
     }
 
-    public static void request(Fragment fragment, Action onGranted, Action onDemied, String... permissions) {
+    public static void request(Fragment fragment, Action<List<String>> onGranted, Action<List<String>> onDenied, String... permissions) {
         AndPermission.with(fragment)
                 .runtime()
                 .permission(permissions)
                 .onGranted(onGranted)
-                .onDenied(onDemied)
+                .onDenied(onDenied)
                 .start();
     }
 }
